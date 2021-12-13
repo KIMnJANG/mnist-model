c4b0a9dc335b6455ad09aaa9707bfca441435a30
suwan <jsuwan961205@gmail.com>
2021-12-13 10:39:07 +0900

    reformat cd

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,6 @@
-FROM tensorflow/tensorflow
+FROM gcr.io/deeplearning-platform-release/tf2-gpu.2-6
 
-ARG GITHUB_OWNER_ARG
-ENV GITHUB_OWNER=${GITHUB_OWNER_ARG}
+WORKDIR /src
 
-ARG GITHUB_REPO_ARG
-ENV GITHUB_REPO=${GITHUB_REPO_ARG}
-
-ARG GITHUB_WORKFLOW_ARG
-ENV GITHUB_WORKFLOW=${GITHUB_WORKFLOW_ARG}
-
-ARG GITHUB_TOKEN_ARG
-ENV GITHUB_TOKEN=${GITHUB_TOKEN_ARG}
-
-ARG GCS_BUCKET_ARG
-ENV GCS_BUCKET=${GCS_BUCKET_ARG}
-
-ARG MODEL_TAG_ARG
-ENV MODEL_TAG=${MODEL_TAG_ARG}
-
-RUN apt-get -y update
-RUN apt-get -y install git
-RUN /usr/bin/python3 -m pip install --upgrade pip
-RUN pip install dvc
-RUN pip install 'dvc[gs]'
-RUN pip install typing
-
-RUN mkdir -p /app
-ADD train.py /app/
-ADD utils.py /app/
-
-# Sets up the entry point to invoke the trainer.
-ENTRYPOINT ["python", "app/train.py"]
-# FROM tensorflow/tensorflow
-
-# ADD train.py /
+ADD train.py /src
+ADD utils.py /src
--- train.py
+++ train.py
@@ -9,16 +9,17 @@ from tensorflow.python.lib.io import file_io
 import datetime
 from utils import request_deploy_api
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--units', default=64, type=float)
-    parser.add_argument('--optimizer', default='adam', type=str)
+    parser.add_argument("--units", default=64, type=float)
+    parser.add_argument("--optimizer", default="adam", type=str)
     # parser.add_argument('--learning_rate', default=0.01, type=float)
     # parser.add_argument('--dropout', default=0.2, type=float)
     # parser.add_argument('--epochs', default=5, type=int)
-    parser.add_argument('--deploy', default=False, type=bool)
+    parser.add_argument("--deploy", default=False, type=bool)
     return parser.parse_args()
-  
+
 
 def load_data():
     # with dvc.api.open(
@@ -34,56 +35,66 @@ def load_data():
     # return (train_x, train_y), (test_x, test_y)
     return tf.keras.datasets.mnist.load_data()
 
+
 def normalize(data):
-  return data / 225.0
+    return data / 225.0
+
 
 def get_model(args):
-  model = tf.keras.models.Sequential([
-      tf.keras.layers.Flatten(input_shape=(28, 28)),
-      tf.keras.layers.Dense(args.units, activation='relu'),
-      tf.keras.layers.Dense(10, activation='softmax')
-    ])
-  model.compile(optimizer=args.optimizer,
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-  return model
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(args.units, activation="relu"),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
+    model.compile(
+        optimizer=args.optimizer,
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+    return model
+
 
 def train():
 
     args = get_args()
     model = get_model(args)
-    
+
     (train_x, train_y), (test_x, test_y) = load_data()
     train_x, test_x = normalize(train_x), normalize(test_x)
-    
+
     print("Training...")
     training_history = model.fit(train_x, train_y, validation_split=0.2, epochs=10)
-    
+
     if args.deploy:
-      deploy_model(model, args)
+        deploy_model(model, args)
+
 
 def arg_to_str(args):
-  return "_".join([f"{x[0]}_{x[1]}" for x in vars(args).items()][:-1])
+    return "_".join([f"{x[0]}_{x[1]}" for x in vars(args).items()][:-1])
+
 
 def deploy_model(model, args):
-  gcp_bucket = os.getenv("GCS_BUCKET")
-  bucket_path = os.path.join(gcp_bucket, "mnist")
-  save_path = f"{arg_to_str(args)}.h5"
-  print(f"saving model {save_path}")
-  model.save(save_path)
+    gcp_bucket = os.getenv("GCS_BUCKET")
+    bucket_path = os.path.join(gcp_bucket, "mnist")
+    save_path = f"{arg_to_str(args)}.h5"
+    print(f"saving model {save_path}")
+    model.save(save_path)
 
-  gs_path = os.path.join(bucket_path, save_path)
-  with file_io.FileIO(save_path, mode='rb') as input_file:
-    with file_io.FileIO(gs_path, mode='wb+') as output_file:
-      output_file.write(input_file.read())
-  print(f"model save success!")
+    gs_path = os.path.join(bucket_path, save_path)
+    with file_io.FileIO(save_path, mode="rb") as input_file:
+        with file_io.FileIO(gs_path, mode="wb+") as output_file:
+            output_file.write(input_file.read())
+    print(f"model save success!")
 
-  request_deploy_api(gs_path)
-  print(f"Trigger Deploy success!")
+    request_deploy_api(gs_path)
+    print(f"Trigger Deploy success!")
 
-  # slack_url = os.getenv("WEB_HOOK_URL")
-  # if slack_url != None:
-  #     send_message_to_slack(slack_url, acc, loss, training_time, gs_path)
+    # slack_url = os.getenv("WEB_HOOK_URL")
+    # if slack_url != None:
+    #     send_message_to_slack(slack_url, acc, loss, training_time, gs_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     train()
